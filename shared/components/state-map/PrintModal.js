ffeaedce892a5ffcdd33d1feaec3c5a1326f8dc8
polbac <pablob@projectcor.com>
2021-02-02 18:38:29 -0300

    page

--- shared/components/state-map/PrintModal.js
+++ shared/components/state-map/PrintModal.js
@@ -7,8 +7,16 @@ import ShapeOne from './assets/images/modal/shape-1.svg';
 import ShapeTwo from './assets/images/modal/shape-2.svg';
 import GdpIcon from './assets/images/map/gdp-icon.svg';
 import JobIcon from './assets/images/map/job-icon.svg';
+import LeftArrow from './assets/images/map/left-arrow.svg';
+import ShapeBottom from './assets/images/modal/bottom-modal.svg';
 import s from './assets/scss/main.scss';
 import { printDocument } from './utils';
+
+const PAGE_HEIGHT = 1298;
+
+function range(start, end) {
+  return Array(end - start + 1).fill().map((_, idx) => start + idx);
+}
 
 export default class PrintModal extends React.Component {
 
@@ -16,16 +24,34 @@ export default class PrintModal extends React.Component {
     super(props);
     this.state = {
       print: true,
+      totalPages: 0,
+      page: 1,
     };
+  }
+
+  componentDidMount() {
+    this.setState({
+      totalPages: Math.ceil(this.pdfContainer.offsetHeight / PAGE_HEIGHT),
+      page: 1,
+    });
   }
 
   downloadPDF() {
     printDocument(this.props.data);
   }
 
+  clickPage(index) {
+    this.setState({
+      page: index,
+    });
+
+    document.querySelector('#download-container').scroll(0, ((index - 1) * PAGE_HEIGHT));
+  }
+
   render() {
-    const { print } = this.state;
+    const { print, totalPages, page } = this.state;
     const { data, closeModal } = this.props;
+    const pages = range(1, totalPages);
 
     return ReactDOM.createPortal(
       <div className={`${s.modal__print} ${print && s.modal__print__download}`}>
@@ -33,69 +59,70 @@ export default class PrintModal extends React.Component {
           <div className={s.modal__print__action_buttons}>
             <Button onClick={this.downloadPDF.bind(this)}className={s.button}>Download PDF</Button>
             <buton className={s.modal__print__back} onClick={closeModal}>
-            Back
+              <LeftArrow />Back
           </buton>
           </div>
           <div className={s.modal__print__modal_body} id="download-container">
-            <div className={s.modal__print__header}>
-              <div>
-                <span>5G ECONOMIC IMPACT</span>
-                <h3>{data.name}</h3>
+            <div className="pdf-container" ref={el => this.pdfContainer = el}>
+              <div className={s.modal__print__header}>
+                <div>
+                  <span>5G ECONOMIC IMPACT</span>
+                  <h3>{data.name}</h3>
+                </div>
+                <div>
+                  <CTIALogo />
+                </div>
               </div>
-              <div>
-                <CTIALogo />
-              </div>
-            </div>
 
-            <p className={s.modal__print__impact}>
-            The 5G Economy will have a significant impact on America’s cities
-            and towns, large and small. Over the next ten years, we will see
-            benefits across the country, including <b>4.6M</b> new jobs and
-            <b>$1.7T</b> in economic growth.
-          </p>
+              <p className={s.modal__print__impact}>
+              The 5G Economy will have a significant impact on America’s cities
+              and towns, large and small. Over the next ten years, we will see
+              benefits across the country, including <b>4.6M</b> new jobs and
+              <b>$1.7T</b> in economic growth.
+            </p>
 
-            <div className={s.modal__print__cards}>
-              <div className={s.modal__print__cards__shape}>
-                <ShapeTwo />
-              </div>
-              <div className={s.map__info__content__list__boxes}>
-                {data && data.gdp && data.gdp.sum_format && (
-                <div className={`${s.content__box} ${s.box__blue}`}>
-                  <div className={s.row}>
-                    <div className={s.content__box__icon}>
-                      <GdpIcon />
-                    </div>
-                    <div className={s.content__box__title}>
-                      {data.gdp.sum_format}
-                    </div>
-                    <div className={s.content__box__ttext}>
+              <div className={s.modal__print__cards}>
+                <div className={s.modal__print__cards__shape}>
+                  <ShapeTwo />
+                </div>
+                <div className={s.map__info__content__list__boxes}>
+                  {data && data.gdp && data.gdp.sum_format && (
+                  <div className={`${s.content__box} ${s.box__blue}`}>
+                    <div className={s.row}>
+                      <div className={s.content__box__icon}>
+                        <GdpIcon />
+                      </div>
+                      <div className={s.content__box__title}>
+                        {data.gdp.sum_format}
+                      </div>
+                      <div className={s.content__box__ttext}>
                       GDP GROWTH STATEWIDE
                     </div>
+                    </div>
                   </div>
-                </div>
               )}
-                {data && data.job && data.job.sum_format && (
-                <div
-                  className={`${s.content__box} ${s.content__box__box_green}`}
-                >
-                  <div className={s.row}>
-                    <div className={s.content__box__icon}>
-                      <JobIcon />
-                    </div>
-                    <div className={s.content__box__title}>
-                      {data.job.sum_format}
-                    </div>
-                    <div className={s.content__box__ttext}>
+                  {data && data.job && data.job.sum_format && (
+                  <div
+                    className={`${s.content__box} ${s.content__box__box_green}`}
+                  >
+                    <div className={s.row}>
+                      <div className={s.content__box__icon}>
+                        <JobIcon />
+                      </div>
+                      <div className={s.content__box__title}>
+                        {data.job.sum_format}
+                      </div>
+                      <div className={s.content__box__ttext}>
                       NEW JOBS STATEWIDE
                     </div>
+                    </div>
                   </div>
-                </div>
               )}
+                </div>
+                <div className={s.modal__print__clear} />
               </div>
-              <div className={s.modal__print__clear} />
-            </div>
 
-            {data &&
+              {data &&
             data.metro_areas &&
             Object.keys(data.metro_areas).length > 0 && (
               <div className={s.modal__print__metro_table}>
@@ -121,29 +148,35 @@ export default class PrintModal extends React.Component {
               </div>
             )}
 
-            {data && data.districts && Object.keys(data.districts).length > 0 && (
-            <div className={s.modal__print__district_table}>
-              <div className={s.modal__print__district_table__shape}>
-                <ShapeOne />
-              </div>
-              <div className={s.modal__print__table_head}>
-                <div className={s.modal__print__table_head__title}>
+              {data && data.districts && Object.keys(data.districts).length > 0 && (
+              <div className={s.modal__print__district_table}>
+                <div className={s.modal__print__district_table__shape}>
+                  <ShapeOne />
+                </div>
+                <div className={s.modal__print__table_head}>
+                  <div className={s.modal__print__table_head__title}>
                   Congressional Districts
                 </div>
-                <div className={s.modal__print__head__item}>GDP Growth</div>
-                <div className={s.modal__print__head__item}>New Jobs</div>
-              </div>
-              {Object.keys(data.districts).map((el, i) => (
-                <div className={s.modal__print__table_items} key={`item-${i}`}>
-                  <div className={s.modal__print__item_title}>
-                    {data.districts[el].name}
-                  </div>
-                  <div>{data.districts[el].gdp.sum_format}</div>
-                  <div>{data.districts[el].job.sum_format}</div>
+                  <div className={s.modal__print__head__item}>GDP Growth</div>
+                  <div className={s.modal__print__head__item}>New Jobs</div>
                 </div>
+                {Object.keys(data.districts).map((el, i) => (
+                  <div className={s.modal__print__table_items} key={`item-${i}`}>
+                    <div className={s.modal__print__item_title}>
+                      {data.districts[el].name}
+                    </div>
+                    <div>{data.districts[el].gdp.sum_format}</div>
+                    <div>{data.districts[el].job.sum_format}</div>
+                  </div>
               ))}
-            </div>
+              </div>
           )}
+            </div>
+          </div>
+          <div className={s.modal__print__navigation}>
+            {pages.map(el => (
+              <button onClick={() => this.clickPage(el)}className={`${s.modal__print__navigation__button} ${el === page && s.modal__print__navigation__button__active}`}>{el}</button>
+            ))}
           </div>
         </div>
       </div>,
--- shared/components/state-map/utils.js
+++ shared/components/state-map/utils.js
@@ -13,10 +13,10 @@ export const isServer = () => typeof process === 'object';
 const PAGE_WIDTH = 1048;
 const PAGE_HEIGHT = 1298;
 const FORMAT_ORIENTATION = 'portrait';
-const MARGIN_VERTICAL = 86
-const MARGIN_HORIZONTAL = 92
+const MARGIN_VERTICAL = 86;
+const MARGIN_HORIZONTAL = 92;
 let firstPage = true;
-const CANVAS_WIDTH = PAGE_WIDTH - (MARGIN_HORIZONTAL*2)
+const CANVAS_WIDTH = PAGE_WIDTH - (MARGIN_HORIZONTAL * 2);
 
 function getBase64Image(imageData, type) {
   // Create canvas
@@ -53,12 +53,12 @@ function addFirstPage(doc) {
       resolve();
     });
     img.src = FirstPageImage;
-    
+
   });
 }
 
 function addSecondBlock(doc, data) {
-  return new Promise(resolve => {
+  return new Promise((resolve) => {
     const img = new Image();
 
     img.setAttribute('crossorigin', 'anonymous');
@@ -70,19 +70,19 @@ function addSecondBlock(doc, data) {
       doc.addImage(dataUrl, 'PNG', MARGIN_HORIZONTAL + 20 + imageSize, MARGIN_VERTICAL + 290, imageSize, imageSize / imageRes);
 
       doc.setFont('SourceSansBold');
-      doc.setTextColor(182,199,0)
+      doc.setTextColor(182, 199, 0);
       doc.setFontSize(100);
       doc.text(data.job.sum_format, MARGIN_HORIZONTAL + imageSize + 20 + (imageSize / 2), MARGIN_VERTICAL + 290 + 190, {
-        align: 'center'
-    });
+        align: 'center',
+      });
       resolve();
     });
     img.src = BoxJobs;
-  })
+  });
 }
 
 function addFirstBlock(doc, data) {
-  return new Promise(resolve => {
+  return new Promise((resolve) => {
     const img = new Image();
 
     img.setAttribute('crossorigin', 'anonymous');
@@ -94,20 +94,20 @@ function addFirstBlock(doc, data) {
       doc.addImage(dataUrl, 'PNG', MARGIN_HORIZONTAL, MARGIN_VERTICAL + 290, imageSize, imageSize / imageRes);
 
       doc.setFont('SourceSansBold');
-      doc.setTextColor(67,159,161)
+      doc.setTextColor(67, 159, 161);
       doc.setFontSize(100);
       doc.text(data.gdp.sum_format, MARGIN_HORIZONTAL + (imageSize / 2), MARGIN_VERTICAL + 290 + 190, {
-        align: 'center'
+        align: 'center',
       });
       resolve();
     });
     img.src = BoxState;
-  })
+  });
 }
 
 function addHeader(doc, title, y) {
   doc.setFont('SourceSansBold');
-  doc.setTextColor(0,0,0)
+  doc.setTextColor(0, 0, 0);
   doc.setFontSize(25);
   doc.text(title, MARGIN_HORIZONTAL, y);
 
@@ -117,21 +117,21 @@ function addHeader(doc, title, y) {
   doc.setFontSize(16);
   doc.text('New Jobs', MARGIN_HORIZONTAL + 811, y);
 
-  doc.setDrawColor(67,159,161);
-  doc.line(MARGIN_HORIZONTAL - 20, y + 17, MARGIN_HORIZONTAL + CANVAS_WIDTH + 20, y + 17)
+  doc.setDrawColor(67, 159, 161);
+  doc.line(MARGIN_HORIZONTAL - 20, y + 17, MARGIN_HORIZONTAL + CANVAS_WIDTH + 20, y + 17);
 }
 
-const LINE_HEIGHT = 35
+const LINE_HEIGHT = 35;
 
 function addField(f, sw, y, doc) {
   if (sw) {
-    doc.setDrawColor(0)
-    doc.setFillColor(240,240,240)
-    doc.rect(MARGIN_HORIZONTAL - 20, y, CANVAS_WIDTH + 40, LINE_HEIGHT, 'F')
+    doc.setDrawColor(0);
+    doc.setFillColor(240, 240, 240);
+    doc.rect(MARGIN_HORIZONTAL - 20, y, CANVAS_WIDTH + 40, LINE_HEIGHT, 'F');
   }
 
   doc.setFont('SourceSansBold');
-  doc.setTextColor(0,0,0)
+  doc.setTextColor(0, 0, 0);
   doc.setFontSize(16);
   doc.text(f.name, MARGIN_HORIZONTAL, y + 20);
 
@@ -140,97 +140,106 @@ function addField(f, sw, y, doc) {
   doc.text(f.gdp.sum_format, MARGIN_HORIZONTAL + 611, y + 20);
 
   doc.setFontSize(16);
-  doc.text(f.job.sum_format, MARGIN_HORIZONTAL + 811, y+ 20);
+  doc.text(f.job.sum_format, MARGIN_HORIZONTAL + 811, y + 20);
 }
 
 function addShape1(doc) {
-  return new Promise(resolve => {
+  return new Promise((resolve) => {
     const img = new Image();
+
     img.setAttribute('crossorigin', 'anonymous');
     img.addEventListener('load', (event) => {
       const dataUrl = getBase64Image(event.currentTarget, 'PNG');
+
       doc.addImage(dataUrl, 'PNG', 40, 365, 58, 198);
 
       resolve();
     });
     img.src = Shape2;
-  })
+  });
 }
 
 function addShape2(doc) {
-  return new Promise(resolve => {
+  return new Promise((resolve) => {
     const img = new Image();
+
     img.setAttribute('crossorigin', 'anonymous');
     img.addEventListener('load', (event) => {
       const dataUrl = getBase64Image(event.currentTarget, 'PNG');
+
       doc.addImage(dataUrl, 'PNG', 939, 931, 58, 220);
       resolve();
     });
     img.src = Shape1;
-  })
+  });
 }
 
 function addLogo(doc) {
-  return new Promise(resolve => {
+  return new Promise((resolve) => {
     const img = new Image();
+
     img.setAttribute('crossorigin', 'anonymous');
     img.addEventListener('load', (event) => {
       const dataUrl = getBase64Image(event.currentTarget, 'PNG');
+
       doc.addImage(dataUrl, 'PNG', 847, 111, 124, 66);
       resolve();
     });
     img.src = Logo;
-  })
+  });
 }
 
 function addMark(doc) {
-  return new Promise(async resolve => {
+  return new Promise(async (resolve) => {
     if (firstPage) {
-      firstPage = false
-      await addShape1(doc)
-      await addShape2(doc)
-      resolve()
-      return
+      firstPage = false;
+      await addShape1(doc);
+      await addShape2(doc);
+      resolve();
+      return;
     }
 
-    await addLogo(doc)
-    await addShape2(doc)
-    resolve()
-  })
+    await addLogo(doc);
+    await addShape2(doc);
+    resolve();
+  });
 }
 
 export async function asyncForEach(array, callback) {
   for (let index = 0; index < array.length; index++) {
-      await callback(array[index], index);
+    await callback(array[index], index);
   }
 }
 
 function addTable(doc, title, fields, _y) {
-  return new Promise(async resolve => {
-    let y = _y
-    addHeader(doc, title, y)
-    y += 17
-    let sw = true
-    
-    await asyncForEach(Object.keys(fields), async(key, index) => {
-      const f = fields[key]
-        addField(f, sw, y, doc)
-        y += LINE_HEIGHT
+  return new Promise(async (resolve) => {
+    let y = _y;
 
-        sw = !sw
+    addHeader(doc, title, y);
+    y += 17;
+    let sw = true;
 
-        if (y > PAGE_HEIGHT - 100 && index < Object.keys(fields).length -1) {
-          await addMark(doc)
-          const { newY, newDoc } = addNewPage(doc)
-          y = newY
-          doc = newDoc
-          addHeader(doc, title, y)
-          y += 17
-        }
-    })
-    
-    resolve(y)
-  })
+    await asyncForEach(Object.keys(fields), async (key, index) => {
+      const f = fields[key];
+
+      addField(f, sw, y, doc);
+      y += LINE_HEIGHT;
+
+      sw = !sw;
+
+      if (y > PAGE_HEIGHT - 100 && index < Object.keys(fields).length - 1) {
+        await addMark(doc);
+        const { newY, newDoc } = addNewPage(doc);
+
+        y = newY;
+        doc = newDoc;
+        addHeader(doc, title, y);
+        y += 17;
+      }
+    });
+
+    resolve(y);
+  });
 }
 
 function addNewPage(doc) {
@@ -238,14 +247,14 @@ function addNewPage(doc) {
     unit: 'px',
     format: [PAGE_WIDTH, PAGE_HEIGHT],
     orientation: FORMAT_ORIENTATION,
-  })
+  });
 
-  return {newDoc, newY : MARGIN_VERTICAL + 150}
+  return { newDoc, newY: MARGIN_VERTICAL + 150 };
 }
 
 export const printDocument = async (data) => {
-  firstPage = true
-  console.log(data)
+  firstPage = true;
+
   let doc = new jsPDF({
     unit: 'px',
     format: [PAGE_WIDTH, PAGE_HEIGHT],
@@ -255,35 +264,36 @@ export const printDocument = async (data) => {
   loadFonts(doc);
 
   await addFirstPage(doc);
-  
+
   doc.setFont('SourceSansBold');
   doc.setFontSize(72);
-  doc.setTextColor(0,0,0)
+  doc.setTextColor(0, 0, 0);
   doc.text(data.name, MARGIN_HORIZONTAL, MARGIN_VERTICAL + 125);
 
   await addFirstBlock(doc, data);
-  
+
   if (data.job && data.job.sum_format) {
     await addSecondBlock(doc, data);
   }
-  let districtsY = 800
-  
-  if ( data.districts && Object.keys(data.districts).length > 0) {
+  let districtsY = 800;
+
+  if (data.districts && Object.keys(data.districts).length > 0) {
     districtsY = await addTable(doc, 'Congressional Districts', data.districts, 800);
 
   }
 
   if (Object.keys(data.metro_areas).length > 0) {
     if (districtsY > PAGE_HEIGHT - 100) {
-      await addMark(doc)
-      const { newY, newDoc} = addNewPage(doc)
-      doc = newDoc
-      districtsY = newY
+      await addMark(doc);
+      const { newY, newDoc } = addNewPage(doc);
+
+      doc = newDoc;
+      districtsY = newY;
     }
     const metroAreasY = await addTable(doc, 'Metro Areas', data.metro_areas, districtsY + 50);
-   }
+  }
 
-   await addMark(doc)  
+  await addMark(doc);
 
   const filename = `CTIA_${data.name}.pdf`;
 
